104fe0590013ffd6f3163d772bb022098b54195b
husnainkhurshiid <muhammadhusnainkh@gmail.com>
2023-12-26 22:35:26 +0500

    fix: (chromedriver as service)

--- warriortrading.py
+++ warriortrading.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,13 +20,11 @@ def convert_to_numeric(value):
         return float(value)
 
 def scrape_data():
-    chrome_path = "chromedriver.exe"
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    chrome_service = ChromeService(chrome_path)
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(service=Service(), options=chrome_options)
 
     url = "https://www.warriortrading.com/day-trading-watch-list-top-stocks-to-watch/"
     driver.get(url)
