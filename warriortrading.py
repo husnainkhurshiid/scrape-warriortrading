44d1c5183d429dace859677b402f5d9cbdf2bae8
husnainkhurshiid <muhammadhusnainkh@gmail.com>
2024-01-10 10:39:11 +0500

    fix:(remove code to handle popup)

--- warriortrading.py
+++ warriortrading.py
@@ -31,12 +31,6 @@ def scrape_data():
 
     time.sleep(5)
 
-    button_xpath = "//div[@aria-label='marketing and promotions']/div/div[2]"
-    button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, button_xpath))
-    )
-    button.click()
-
     time.sleep(5)
 
     rows = driver.find_elements(By.XPATH, "//div[@class='overflow-x-auto']/div[2]")
